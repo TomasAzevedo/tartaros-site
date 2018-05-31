c440af0e719823e25b67ff3e803f7c6d79d4d72e
Tomas Azevedo <tomas.az@gmail.com>
2018-05-31 18:06:25 -0300

    Sabore + parallax + smoth

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 import { SharedModule } from '../shared/shared.module';
 import { IntroducaoComponent } from './introducao/introducao.component';
+import { SaboresComponent } from './sabores/sabores.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule
   ],
-  declarations: [BarraNavegacaoComponent, IntroducaoComponent],
+  declarations: [BarraNavegacaoComponent, IntroducaoComponent, SaboresComponent],
   exports: [
     BarraNavegacaoComponent,
-    IntroducaoComponent
+    IntroducaoComponent,
+    SaboresComponent
   ],
   schemas: [ NO_ERRORS_SCHEMA ]
 })
---
+++ src/app/core/sabores/sabores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sabores',
+  templateUrl: './sabores.component.html',
+  styleUrls: ['./sabores.component.scss']
+})
+export class SaboresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
